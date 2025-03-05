3c66816455b1e37e574a036a01939fd6131acc0d
${'Julian Alvarado'} <juanalsa@gmail.com>
2025-03-04 22:32:02 -0500

    :wrench: Initialize fix-now-api project with Spring Boot, H2, and Redis support

---
+++ src/main/java/com/fixnow/api/FixNowApiApplication.java
@@ -0,0 +1,13 @@
+package com.fixnow.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixNowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixNowApiApplication.class, args);
+	}
+
+}
